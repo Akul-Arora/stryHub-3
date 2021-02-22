97bcbb30b39d37b5b508637a94a8c7eaadb092f6
anshika4 <72254585+anshika4@users.noreply.github.com>
2021-02-22 16:07:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,135 +1,39 @@
+
 import React from 'react';
-import { StyleSheet, Text,TouchableOpacity,View,TextInput,KeyboardAvoidingView ,ToastAndroid} from 'react-native';
-import db from '../config'
+import {Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import ReadScreen from './screens/Readstory';
+import WriteScreen from './screens/Writestory';
 
-export default class WriteScreen extends React.Component{
-constructor(){
-super();
-this.state={
-TitleStory:'' ,
-AuthorStory:'' ,
-Writestory: '',
-}
-}
-
-submitStory = () => {
-db.collection('story').add({
-TitleStory: this.state.TitleStory,
-AuthorStory: this.state.AuthorStory,
-Writestory: this.state.Writestory,
-});
-this.setState({
-title: '',
-author: '',
-storyText: '',
-}); 
-ToastAndroid.show('STORY SUBMITTED', ToastAndroid.SHORT)
-};
-
-
+export default class App extends React.Component {
 render(){
 return (
-<KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-<Text style={styles.header}>Story Hub</Text>
+<AppContainer/>
+);
+}
+}
 
-<TextInput
-style={styles.TitleStory}
-placeholder='write Story Tittle'
-onChangeText={(text)=>{
-this.setState({
-TitleStory: text
-})
-}}
-value={this.state.TitleStory}/>
-
-<TextInput
-style={styles.AuthorStory}
-placeholder='write Author of the story'
-onChangeText={(text)=>{
-this.setState({
-AuthorStory: text
-})
-}}
-value={this.state.AuthorStory}/>   
-
-
-<TextInput 
-style={styles.Writestory}
-placeholder='write story here'
-onChangeText={(text)=>{ 
-this.setState({
-Writestory:text
-
-})
-}}
-multiline={true}
-value={this.state.Writestory}
-/>
-
-<TouchableOpacity style={styles.submit} onPress = {this.submitStory}>
-<Text>Sumbit</Text>   
-</TouchableOpacity>
-
-</KeyboardAvoidingView>
+const TabNavigator=createBottomTabNavigator({
+Write:{screen:WriteScreen},
+Read:{screen:ReadScreen}
+},
+{
+defaultNavigationOptions:({navigation})=>({
+tabBarIcon: ()=>{
+const routeName=navigation.state.routeName;
+console.log (routeName);
+if(routeName==='Read'){
+return(
+<Image style={{width:40,height:40}} source={require('./assets/read.png')}/>
+)
+}else if(routeName==='Write'){
+return(
+<Image style={{width:40,height:40}} source={require('./assets/write.png')}/>
 )
 }
 }
-
-const styles = StyleSheet.create({
-container: { 
-flex: 1,
-justifyContent: 'center', 
-alignItems: 'center' 
-}, 
-submit: {
-marginTop:15, 
-marginLeft:80,    
-height: 60,
-width: 200,
-alignItems: 'center',
-justifyContent: 'center',
-color: 'white',
-borderWidth: 4,
-borderColor: 'indigo',
-backgroundColor: '#ff69b4',
-},
-
-Writestory: {
-marginTop: 50, 
-width: '80%',
-alignSelf: 'center', 
-height: 40, 
-textAlign: 'center', 
-borderWidth: 4, 
-height:200,
-borderColor: 'pink',
-},
-
-TitleStory: {
-marginTop: 50, 
-width: '80%',
-alignSelf: 'center', 
-height: 40, 
-textAlign: 'center', 
-borderWidth: 4, 
-borderColor: 'pink',
-},
-
-AuthorStory: {
-marginTop: 50, 
-width: '80%',
-alignSelf: 'center', 
-height: 40, 
-textAlign: 'center', 
-borderWidth: 4, 
-borderColor: 'pink',
-},
-
-header: {
-marginTop:15, 
-backgroundColor : '#ff1493',  
-color : 'white', 
-fontSize : 50,
-textAlign : 'center'
-},
-});
+})
+}
+);
+const AppContainer= createAppContainer(TabNavigator)
